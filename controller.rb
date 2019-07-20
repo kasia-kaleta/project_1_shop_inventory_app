cfa9e1084f7cdf2b58fe57353a3a9225ac942820
kat3005 <kasia.alicja@gmail.com>
2019-07-20 20:17:01 +0100

    Adds category_index file

--- controller.rb
+++ controller.rb
@@ -5,7 +5,12 @@ require_relative('models/categories')
 require_relative('models/products')
 also_reload('./models/*')
 
-get '/brand_index' do
+get '/brands' do
   @brands = Brand.all
   erb(:brand_index)
+end
+
+get '/categories' do
+  @categories = Category.all
+  erb(:category_index)
 end
