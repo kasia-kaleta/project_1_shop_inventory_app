c383fcca94a520d40b9f7008d4e0e0f8cd7b3441
kat3005 <kasia.alicja@gmail.com>
2019-07-20 15:11:16 +0100

    Adds self.all method to category class

--- models/categories.rb
+++ models/categories.rb
@@ -12,10 +12,17 @@ class Category
   end
 
   def save()
-    sql = "INSERT INTO brands (name) VALUES ($1) RETURNING id"
+    sql = "INSERT INTO categories (name) VALUES ($1) RETURNING id"
     values = [@name, @id]
     result = SqlRunner.run(sql, values)
     @id = result.first['id']
+  end
+
+  def self.all()
+    sql = "SELECT * FROM categories"
+    categories = SqlRunner.run(sql)
+    result = categories.map { |category| Category.new(category) }
+    return result
   end
 
 end
