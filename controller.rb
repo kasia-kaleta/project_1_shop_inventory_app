81efd81c41cc99a31ebbe6879e7f859ad45fad0a
kat3005 <kasia.alicja@gmail.com>
2019-07-20 20:06:42 +0100

    Adds brand index

--- controller.rb
+++ controller.rb
@@ -4,3 +4,8 @@ require_relative('models/brands')
 require_relative('models/categories')
 require_relative('models/products')
 also_reload('./models/*')
+
+get '/brand_index' do
+  @brands = Brand.all
+  erb(:brand_index)
+end
