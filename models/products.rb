c0795a2d502b982ce20d000cc3bfc1c8b714c2a1
kat3005 <kasia.alicja@gmail.com>
2019-07-20 15:27:19 +0100

    Adds delete method to product class

--- models/products.rb
+++ models/products.rb
@@ -39,4 +39,11 @@ class Product
     SqlRunner.run(sql, values)
   end
 
+  def delete()
+    sql = "DELETE FROM products
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
 end
