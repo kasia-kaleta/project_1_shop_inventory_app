1b6b349516585fcca52b87f87512e2cec41d031b
kat3005 <kasia.alicja@gmail.com>
2019-07-22 16:04:16 +0100

    Adds new products to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,28 +29,28 @@ brand4 = Brand.new({
   'info' => 'Rimmel is a British cosmetics brand, owned by Coty, Inc. The House of Rimmel was founded by Eugène Rimmel as a perfumery in 1834 in Regent Street, London, England. Within a year of opening, Rimmel came to create many make-up products including his best-known mascara.'
   })
 
-  brand1.save()
-  brand2.save()
-  brand3.save()
-  brand4.save()
+brand1.save()
+brand2.save()
+brand3.save()
+brand4.save()
 
-  category1 = Category.new({
-    'name' => 'Complexion',
-    'description' => 'This category contains face products like foundations, powders, blushes, bronzers, highlighters, etc.'})
+category1 = Category.new({
+  'name' => 'Complexion',
+  'description' => 'This category contains face products like foundations, powders, blushes, bronzers, highlighters, etc.'})
 
-  category2 = Category.new({
+category2 = Category.new({
     'name' => 'Eyes',
     'description' => 'This category contains eye makeup products like mascaras, eye liners, eye shadows, eye brow pencils, etc.'})
 
-  category3 = Category.new({
+category3 = Category.new({
     'name' => 'Lips',
     'description' => 'This category contains lip makeup products like lip liners, lipsticks, lip glosses, etc.'})
 
-  category1.save()
-  category2.save()
-  category3.save()
+category1.save()
+category2.save()
+category3.save()
 
-  product1 = Product.new({
+product1 = Product.new({
     'name' => 'Volume Million Lashes Mascara',
     'description' => 'Millionizer Brush - a multitude of bristles separate lashes for a fanned-out effect. Excess Wiper - in-built wiper system removes excess mascara, wiping brush free of clumps.',
     'quantity' => 50,
@@ -60,4 +60,104 @@ brand4 = Brand.new({
     'category_id' => category2.id
     })
 
-    product1.save()
+product2 = Product.new({
+    'name' => 'Hoola Bronzer',
+    'description' => 'Bronzing powder Hoola is the perfect contour shade to acomplish sun-kissed glow. Great for contouring, soft, natural-bristle brush included.',
+    'quantity' => 20,
+    'buying_cost' => 17.00,
+    'selling_price' => 26.00,
+    'brand_id' => brand1.id,
+    'category_id' => category1.id
+    })
+
+product3 = Product.new({
+    'name' => 'Precisely, My Brow pencil',
+    'description' => 'Waterproof mechanical eyebrow pencil with twist-up tip and built-in spoolie.',
+    'quantity' => 5,
+    'buying_cost' => 14.50,
+    'selling_price' => 21.50,
+    'brand_id' => brand1.id,
+    'category_id' => category2.id
+    })
+
+product4 = Product.new({
+    'name' => 'Studio Fix Fluid Foundation',
+    'description' => 'The oil-free foundation offers a shine-control base with a broad spectrum UVA /UVB SPF 15 and PA++ protection.',
+    'quantity' => 50,
+    'buying_cost' => 19.50,
+    'selling_price' => 27.00,
+    'brand_id' => brand3.id,
+    'category_id' => category1.id
+      })
+
+product5 = Product.new({
+    'name' => 'Wonder Luxe Volume Mascara',
+    'description' => 'Conditioning mascara,infused with argan, maracuja, marula & camellia oils.Smudge-proof formula, dermatologist & ophthalmologist tested',
+    'quantity' => 50,
+    'buying_cost' => 4.20,
+    'selling_price' => 8.99,
+    'brand_id' => brand4.id,
+    'category_id' => category2.id
+        })
+
+product6 = Product.new({
+    'name' => 'Ruby Woo Lipstick',
+    'description' => 'The iconic product that made M·A·C famous. This creamy rich formula features high colour payoff in a no-shine matte finish.',
+    'quantity' => 30,
+    'buying_cost' => 14.20,
+    'selling_price' => 17.50,
+    'brand_id' => brand3.id,
+    'category_id' => category3.id
+        })
+
+product7 = Product.new({
+    'name' => 'Stay Matte Liquid Lip Colour',
+    'description' => 'Long-lasting lipstick formula with intense colour pay off. Velvet matte finish',
+    'quantity' => 40,
+    'buying_cost' => 4.20,
+    'selling_price' => 5.99,
+    'brand_id' => brand4.id,
+    'category_id' => category3.id
+        })
+
+product8 = Product.new({
+    'name' => 'High Beam Highlighter',
+    'description' => 'Satiny pink liquid highlighter that accents cheek & brow bones for a dewy, radiant glow.',
+    'quantity' => 0,
+    'buying_cost' => 18.20,
+    'selling_price' => 22.00,
+    'brand_id' => brand1.id,
+    'category_id' => category1.id
+        })
+
+product9 = Product.new({
+    'name' => 'Color Riche Plump and Shine Lipstick',
+    'description' => 'Highly pigmented, lip-plumping formula. Hydrating formula with high-shine finish',
+    'quantity' => 60,
+    'buying_cost' => 6.90,
+    'selling_price' => 8.99,
+    'brand_id' => brand2.id,
+    'category_id' => category3.id
+        })
+
+product10 = Product.new({
+    'name' => 'Stay Matte Pressed Powder',
+    'description' => 'Skin-mattifying, shine-controlling powder. Up to 5 hours of wear',
+    'quantity' => 35,
+    'buying_cost' => 3.20,
+    'selling_price' => 3.99,
+    'brand_id' => brand4.id,
+    'category_id' => category1.id
+        })
+
+
+product1.save()
+product2.save()
+product3.save()
+product4.save()
+product5.save()
+product6.save()
+product7.save()
+product8.save()
+product9.save()
+product10.save()
